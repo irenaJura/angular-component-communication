0631c63bb7dfaa3083da1a529b6a3a514957c886
Irena Jurasek <irenaskydive@gmail.com>
2022-12-09 16:59:06 +0100

    changed to alternative layout

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,12 +8,15 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './edit/product-edit.component';
 
 import { ProductEditGuard } from './edit/product-edit-guard.service';
+import { ProductShellComponent } from './product-shell/product-shell.component';
+import { ProductShellListComponent } from './product-shell/product-shell-list.component';
+import { ProductShellDetailComponent } from './product-shell/product-shell-detail.component';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: ProductListComponent },
+      { path: '', component: ProductShellComponent },
       { path: ':id', component: ProductDetailComponent },
       {
         path: ':id/edit',
@@ -25,7 +28,10 @@ import { ProductEditGuard } from './edit/product-edit-guard.service';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductShellComponent,
+    ProductShellListComponent,
+    ProductShellDetailComponent
   ]
 })
 export class ProductModule { }
