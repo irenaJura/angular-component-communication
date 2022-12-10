a353dcac2367d9643aba43eb7f843ffc03df6af0
Irena Jurasek <irenaskydive@gmail.com>
2022-12-10 14:42:07 +0100

    add another subscriber to subject as observer

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { startWith } from 'rxjs';
+import { ProductService } from '../product.service';
 
 @Component({
     templateUrl: './product-shell.component.html'
@@ -7,9 +9,15 @@ export class ProductShellComponent implements OnInit {
   pageTitle = 'Products';
   monthCount = 0;
 
-    constructor() { }
+    constructor(private productService: ProductService) { }
 
     ngOnInit() {
+      this.productService.selectedProductChanges$.subscribe({
+        next: (selectedProduct) => {
+         const releaseDate = new Date(selectedProduct.releaseDate);
+         const now = new Date();
+         this.monthCount = now.getMonth() - releaseDate.getMonth() + 12 * (now.getFullYear() - releaseDate.getFullYear());
+        }
+      })
     }
-
 }
